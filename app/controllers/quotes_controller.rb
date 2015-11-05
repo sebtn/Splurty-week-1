5f76e3a3dad1fe8441dd7e4c599cfd0a7caf2352
sebtn <tonomudo@gmail.com>
2015-11-05 20:17:45 +0000

    Validation error

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -8,7 +8,10 @@ class QuotesController < ApplicationController
 	end
 
 	def create
-		Quote.create(quote_params)
+		@quote = Quote.create(quote_params)
+		if @quote.invalid?
+		   flash[:error] = '<strong>Could not save</strong> the data you entered is invalid.'
+		end
 		redirect_to root_path
 	end
 
@@ -17,5 +20,5 @@ class QuotesController < ApplicationController
 	def quote_params
 		params.require(:quote).permit(:saying, :author)
 	end
-end
 
+end
--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,2 +1,4 @@
 class Quote < ActiveRecord::Base
+	validates :saying, :presence => true, :length => { :maximun => 14, :minimum => 3}
+	validates :author, :presence => true, :length => { :maximun => 50, :minimum => 3}
 end
